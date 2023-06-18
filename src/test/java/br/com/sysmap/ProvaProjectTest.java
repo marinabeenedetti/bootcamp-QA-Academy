a768174e9e2d67b8eb683f63d23d6bc85bd71cbb
Marina Benedetti <114014144+marinabeenedetti@users.noreply.github.com>
2023-06-18 19:16:00 -0300

    Api Test
    testes finais combinando resultados da API com o Site. Também realizado ajustes para ser possível buscar por cidades que tem 2 palavras (exemplo joão pessoa)

---
+++ src/test/java/br/com/sysmap/ProvaProjectTest.java
@@ -0,0 +1,79 @@
+package br.com.sysmap;
+
+import br.com.sysmap.driver.Api;
+import br.com.sysmap.driver.Browser;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+
+import static br.com.sysmap.driver.Api.currentWeather;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ProvaProjectTest {
+
+    String city = "João Pessoa"; //Case sensitive
+    @BeforeAll
+    static void inicio() {
+        Browser.openChrome("https://openweathermap.org/");
+    }
+
+   @Test
+    @Order(1)
+    void validateWebsite() {
+        boolean validate = Browser.elementExist(By.xpath("//span[text()='OpenWeather']"));
+        assertTrue(validate);
+        System.out.println("Validado que estamos no site do Weather Map");
+    }
+
+    @Test
+    @Order(2)
+    void searchCity()  {
+
+        Browser.write(By.xpath("//input[@placeholder='Search city']"), city);
+        Browser.waiting(4);
+        Browser.click(By.xpath("//button[@class='button-round dark']"));
+        Browser.waiting(5);
+        Browser.click(By.xpath("//ul[@class='search-dropdown-menu']"));
+        Browser.waiting(1);
+        assertTrue(Browser.elementExist(By.xpath("//h2[text()='"+city+", BR']")));
+        System.out.println("CT1 - Validado que pesquisou a cidade de "+city+", BR");
+    }
+    @Test
+    @Order(3)
+    void validTempCelsius()  {
+        double tempAPI, tempSite;
+        Browser.click(By.xpath("//div[text()='Metric: °C, m/s']"));
+        tempAPI = Api.currentWeatherTemp(city,"metric");
+        Browser.waiting(1);
+        tempSite = Double.parseDouble(Browser.readText(By.xpath("//span[@class='heading']"))
+              .replaceAll("°C"," "));
+        System.out.println("Retorno da API:"+tempAPI+" Valor no site: "+tempSite);
+        boolean validate = (tempAPI == tempSite);
+        assertTrue(validate);
+        System.out.println("CT2 - Validado que a temp em grau Celsius no site é igual a temp da chamada API");
+    }
+
+    @Test
+    @Order(4)
+    void validTempFahrenheit()  {
+        double tempAPI, tempSite;
+        Browser.click(By.xpath("//div[text()='Imperial: °F, mph']"));
+        tempAPI = Api.currentWeatherTemp(city,"Imperial");
+        Browser.waiting(1);
+        tempSite = Double.parseDouble(Browser.readText(By.xpath("//span[@class='heading']"))
+                .replaceAll("°F"," "));
+        System.out.println("Retorno da API:"+tempAPI+" Valor no site: "+tempSite);
+        boolean validate = (tempAPI == tempSite);
+        assertTrue(validate);
+        System.out.println("CT3 - Validado que a temp em grau Fahrenheit no site é igual a temp da chamada API");
+    }
+
+
+
+    @AfterAll
+    static void end() {
+        Browser.closeChrome();
+    }
+
+
+}
--- src/test/java/br/com/sysmap/driver/Api.java
+++ src/test/java/br/com/sysmap/driver/Api.java
@@ -11,17 +11,18 @@ import org.json.JSONObject;
 
 public class Api {
 
-    static String apiKey = "Inserir valor da sua API key";
-    public static String getLatLon(String cidade) {
+    static String apiKey = "875e041c94d845b6a813aaaafddcbef3"; // valor da API do site: https://home.openweathermap.org/api_keys
+    public static String getLatLon(String city) {
         String retorno = null;
+        city = city.replaceAll(" ", "-");
         try (CloseableHttpClient client = HttpClients.createDefault()) {
-            HttpGet get = new HttpGet("http://api.openweathermap.org/geo/1.0/direct?q=" + cidade + "&appid=" + apiKey);
+            HttpGet get = new HttpGet("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey);
             HttpResponse response = client.execute(get);
             String resp = EntityUtils.toString(response.getEntity());
             //            System.out.println(resp);
             JSONObject obj = new JSONObject(resp.replace("[", "").replace("]", ""));
-            System.out.println("valor da lat: " + obj.getBigDecimal("lat"));
-            System.out.println("valor da lon: " + obj.getBigDecimal("lon"));
+           // System.out.println("valor da lat: " + obj.getBigDecimal("lat"));
+           // System.out.println("valor da lon: " + obj.getBigDecimal("lon"));
             retorno = "lat="
                     .concat(String.valueOf(obj.get("lat")))
                     .concat("&lon=")
@@ -32,18 +33,37 @@ public class Api {
         return retorno;
 
     }
-    public static String currentWeather(String cidade) {
+    public static String currentWeather(String city) {
         String retorno = null;
+        city.replaceAll(" ", "-");
         try (CloseableHttpClient client = HttpClients.createDefault()) {
-            String uri = "https://api.openweathermap.org/data/2.5/weather?" + getLatLon(cidade) + "&appid=" + apiKey;HttpGet get = new HttpGet(uri);
+            String uri = "https://api.openweathermap.org/data/2.5/weather?" + getLatLon(city) + "&appid=" + apiKey;HttpGet get = new HttpGet(uri);
             HttpResponse response = client.execute(get);
             String resp = EntityUtils.toString(response.getEntity());
             JSONObject obj = new JSONObject(resp);
-
             System.out.println(obj.getJSONObject("main"));
             JSONObject main = obj.getJSONObject("main");
-            System.out.println("Retorno da api com o valor da temperatura: " + main.get("temp"));
+            System.out.println("Retorno da API com o valor da temperatura: " + main.get("temp"));
             retorno = String.valueOf(obj.get("name"));
+            System.out.println(retorno);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return retorno;
+
+    }
+
+    public static double currentWeatherTemp(String city, String unitsTemp) {
+        double retorno = 0;
+        city.replaceAll(" ", "-");
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            String uri = "https://api.openweathermap.org/data/2.5/weather?" + getLatLon(city) + "&units="+unitsTemp+"&appid=" + apiKey;HttpGet get = new HttpGet(uri);
+            HttpResponse response = client.execute(get);
+            String resp = EntityUtils.toString(response.getEntity());
+            JSONObject obj = new JSONObject(resp);
+            JSONObject main = obj.getJSONObject("main");
+            System.out.println(main.get("temp"));
+            retorno = Math.round(Double.parseDouble(String.valueOf(main.get("temp"))));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
